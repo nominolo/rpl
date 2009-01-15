5e8ba90bf70bd20dfcb273e832fd89f54a235acb
Thomas Schilling <nominolo@googlemail.com>
2009-01-15 17:13:12 +0000

    Add [Arbitrary] instances for [Id].

--- src/RPL/Names.hs
+++ src/RPL/Names.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE FlexibleInstances #-}
 module RPL.Names where
 
 import RPL.Utils.Pretty
 import RPL.Utils.Unique
+
+-- Testing
+import Test.QuickCheck
+import Data.Char ( ord, toUpper )
+
+------------------------------------------------------------------------
 
 -- | An identifier.
 data Id = Id !Unique String
@@ -13,3 +20,28 @@ idString (Id _ n) = n
 instance Pretty Id where
   ppr (Id u v) = text v <> char '_' <> ppr u
 
+------------------------------------------------------------------------
+
+newtype Uppercase a = Uppercase a
+
+instance Arbitrary Id where arbitrary = arbLowercase
+instance Arbitrary (Uppercase Id) where
+  arbitrary = Uppercase `fmap` arbUppercase
+
+arbLowercase = do
+   n <- sized (\n -> elements (take n simpleNames))
+   let i = product (map ord n)
+   return (Id (uniqueFromInt i) n)
+
+arbUppercase = do
+   n <- fmap (map toUpper) $ sized (\n -> elements (take n simpleNames))
+   let i = product (map ord n)
+   return (Id (uniqueFromInt i) n)
+
+simpleNames :: [String]
+simpleNames = go 1
+  where
+    go n = sn n ++ go (n+1)
+    sn :: Int -> [String] -- names of length `n`
+    sn 0 = [""]
+    sn n = [ x:xs | x <- ['a'..'z'], xs <- sn (n-1) ]
