c6169a85504c7b458ec59e0a101bb9b36ef81a7c
Thomas Schilling <nominolo@googlemail.com>
2009-04-11 01:06:32 +0100

    Distinguish multiple identifiers with same base name.
    Calling `genId` twice with the same string argument will now result in
    identifiers that not only have different uniques, but also different
    names.
    
    Conflicts:
    
    	src/RPL/Typecheck/Monad.hs

--- src/RPL/Typecheck/Monad.hs
+++ src/RPL/Typecheck/Monad.hs
@@ -4,20 +4,27 @@ module RPL.Typecheck.Monad (
 
 import RPL.Names
 import RPL.Error
+import RPL.Type
 import RPL.Utils.Monads
 import RPL.Utils.Unique
 
+import Data.Map ( Map )
+import qualified Data.Map as M
+
 ------------------------------------------------------------------------
 
-type TcM a = StrictStateErrorM Int SourceError a
+type TcM a = StrictStateErrorM (Int, Map String Int) SourceError a
 
 runTcM :: TcM a -> Either SourceError a
-runTcM m = runStrictStateErrorM m 100
-
-incCtr :: TcM Int
-incCtr = do s <- getState; modifyState (+1); return s
+runTcM m = runStrictStateErrorM m (100, M.empty)
 
 genId :: String -> TcM Id
 genId disp_name =
-    do i <- incCtr
-       return $ Id (uniqueFromInt i) disp_name
+    do (i, m) <- getState
+       let i' = i + 1
+       let (n, m') = case M.lookup disp_name m of
+             Nothing -> (disp_name, M.insert disp_name 1 m)
+             Just o -> (disp_name ++ "_" ++ show o,
+                        M.update (Just . (1+)) disp_name m)
+       i' `seq` m' `seq` setState (i', m')
+       return $ Id (uniqueFromInt i) n
