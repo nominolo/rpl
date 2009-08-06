3948385a15dc2d4ab638fb9722090c0344eb5b3a
Thomas Schilling <nominolo@googlemail.com>
2009-08-06 16:57:03 +0100

    Small clean-ups.

--- lib/RPL/Typecheck/GrTy/Solve.hs
+++ lib/RPL/Typecheck/GrTy/Solve.hs
@@ -1,20 +1,19 @@
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE FlexibleContexts #-}
-module RPL.Typecheck.GrTy.Solve where
+module RPL.Typecheck.GrTy.Solve ( fastSolveOne, solve ) where
 
 import RPL.Typecheck.GrTy.Types
 import RPL.Typecheck.GrTy.Translate
 import RPL.Typecheck.GrTy.Constraint
 import RPL.Typecheck.GrTy.Unify ( unify )
 import RPL.Utils.Monads
-import RPL.Utils.Unique
-import RPL.Utils.SrcLoc
+import RPL.Utils.Unique ( uniqueFromInt )
+import RPL.Utils.SrcLoc ( noSrcSpan )
 
 import qualified RPL.Syntax as Syn
 import qualified RPL.Names as Syn
 
 import Control.Applicative
-import Data.List ( find )
 
 fastSolveOne :: (Applicative m, MonadIO m, MonadGen NodeId m, MonadError String m) => 
          Env -> ConstraintStore -> ConstrEdge -> m ConstraintStore
@@ -27,7 +26,6 @@ fastSolveOne env cs edge = do
            Inst -> do
              decrForallCount n1
              (cs', n') <- expandForall cs MLF edge
-             --liftIO $ dottyConstraints cs' "expand_forall"
              return (cs', n', n2)
   unify env n1' n2'
   -- TODO: better error message
@@ -48,19 +46,15 @@ sv1 = do
   let x = Syn.Id (uniqueFromInt 2) "x"
   let y = Syn.Id (uniqueFromInt 3) "y"
   let u = noSrcSpan
-  let expr = Syn.ELam u (Syn.VarPat u x) $ -- (Syn.EVar u x)
+  let expr = Syn.ELam u (Syn.VarPat u x) $
               Syn.ELam u (Syn.VarPat u y) $ 
-                Syn.EApp u (Syn.EVar u y) (Syn.EVar u x) 
--- Syn.EApp u
---                  (Syn.EApp u (Syn.EVar u x) (Syn.ELit u (Syn.IntLit 1)))
+                Syn.EApp u (Syn.EVar u y) (Syn.EVar u x)
   r <- runGTM $ do 
          cs <- translate MLF [] expr
          liftIO $ do 
-           --dumpConstraints cs
            dottyConstraints cs "initial"
          cs' <- solve [] cs
          liftIO $ do 
-           --dumpConstraints cs'
            dottyConstraints cs' "final"
- 
+  print r
   return ()
