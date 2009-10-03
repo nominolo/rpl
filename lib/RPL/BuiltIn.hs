578dab5e7d5646633ca17f5b72de5270c8d6a3a3
Thomas Schilling <nominolo@googlemail.com>
2009-10-03 02:07:03 +0100

    Add built-in type env and use temporary fix for Ids+uniques.

--- lib/RPL/BuiltIn.hs
+++ lib/RPL/BuiltIn.hs
@@ -3,12 +3,21 @@ module RPL.BuiltIn where
 import RPL.Names
 import RPL.Type
 import RPL.Utils.Unique
+import RPL.Typecheck.Subst
+
+import Data.List ( foldl' )
 
 typeInt :: TyCon
-typeInt = MkTyCon (Id (uniqueFromInt 1) "Int") 0
+typeInt = MkTyCon (Id (uniqueFromInt 0) "Int") 0
 
 typeChar :: TyCon
-typeChar = MkTyCon (Id (uniqueFromInt 2) "Char") 0
+typeChar = MkTyCon (Id (uniqueFromInt 0) "Char") 0
+
+initialTypeEnv :: Env Id TyCon
+initialTypeEnv =
+  foldl' (\e -> uncurry (extendEnv e)) emptyEnv
+    [ (tyConName tc, tc) | tc <- [ typeInt, typeChar ] ]
 
 typeMaybe :: TyCon
-typeMaybe = MkTyCon (Id (uniqueFromInt 3) "Maybe") 1
+typeMaybe = MkTyCon (Id (uniqueFromInt 0) "Maybe") 1
+
