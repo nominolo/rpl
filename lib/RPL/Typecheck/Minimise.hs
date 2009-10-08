ef5a5f49c9896191fb53b55c0a6473c74bed3017
Thomas Schilling <nominolo@googlemail.com>
2009-10-08 17:18:45 +0100

    Add `minUnsat'` which supports dependencies among constraints.

--- lib/RPL/Typecheck/Minimise.hs
+++ lib/RPL/Typecheck/Minimise.hs
@@ -20,15 +20,35 @@ import qualified Data.Map as M
 --    makes it satisfiable.
 -- 
 minUnsat :: Ord k => 
-    s  
+    s
     -- ^ Initial state of the solver.
  -> (s -> k -> c -> Either a s)
     -- ^ Solve a single constraint.  Either fail or return new state.
  -> M.Map k c
     -- ^ The input constraints.
  -> M.Map k c
-minUnsat state0 run_one try_these = go state0 M.empty try_these
+minUnsat state0 run_one try_these = 
+    minUnsat' state0 run_one pick del try_these
   where
+    pick cs =
+      case M.minViewWithKey cs of
+        Nothing -> Nothing
+        Just ((k,v), cs') -> Just ([(k,v)], cs')
+    del [] cs = cs
+    del ((k,_):ks) cs = M.delete k (del ks cs)
+
+-- -------------------------------------------------------------------
+
+minUnsat' :: Ord k =>
+    s
+ -> (s -> k -> c -> Either a s)
+ -> (cs -> Maybe ([(k, c)], cs))
+ -> ([(k,c)] -> cs -> cs)
+ -> cs
+ -> M.Map k c
+minUnsat' state0 solve_one pick_next delete_cs try_these =
+  go state0 M.empty try_these
+ where
     --
     -- INVARIANT:
     --   run_until_failure state0 min_approx == min_state
@@ -41,20 +61,21 @@ minUnsat state0 run_one try_these = go state0 M.empty try_these
           -- there's no minimal unsatisfiable set.
           M.empty
 
-        Left (last_tried_id, last_tried) ->
+        Left last_tried ->
           -- The last constraint introduced a failure, hence it must
           -- be part of the minimal constraint set.
-          case run_one min_state last_tried_id last_tried of
+          case solve_list min_state last_tried of
             Left _ ->
               -- Adding this one to our min_approx set fails, so
               -- this new set is minimal.  We're done!
-              M.insert last_tried_id last_tried min_approx
+              min_approx `M.union` M.fromList last_tried
+              --M.insert last_tried_id last_tried min_approx
 
             Right s' ->
               -- min_approx is still solvable, try the other
               -- constraints.
-              go s' (M.insert last_tried_id last_tried min_approx)
-                 (M.delete last_tried_id not_tried)
+              go s' (min_approx `M.union` M.fromList last_tried)
+                 (delete_cs last_tried not_tried)
     
     -- Run the solver function on the input constraints until:
     --
@@ -71,10 +92,15 @@ minUnsat state0 run_one try_these = go state0 M.empty try_these
     -- c_k@ were consistent, but adding @c_k+1@ introduced an
     -- inconsistency.
     run_until_failure s not_tried =
-      case M.minViewWithKey not_tried of
+      case pick_next not_tried of
         Nothing -> Right s
-        Just ((k, e), cs') ->
-          case run_one s k e of
-            Left _ -> Left (k, e)
+        Just (es, cs') ->
+          case solve_list s es of
+            Left _ -> Left es
             Right s' -> run_until_failure s' cs'
 
+    solve_list s [] = Right s
+    solve_list s ((k, e):cs) =
+      case solve_one s k e of
+        Left _ -> Left (k, e)
+        Right s' -> solve_list s' cs
